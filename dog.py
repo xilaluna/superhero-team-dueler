d5b8ce67e4f1a1cb38e01df384eac1b5d6f41ded
xiluna <xilaluna2@gmail.com>
2020-11-10 19:16:02 -0800

    Update dog.py

--- dog.py
+++ dog.py
@@ -6,8 +6,13 @@ class Dog:
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
-        print("dog initialized!")
+        print(f'dog initialized!')
+
+    def bark(self):
+        print(f'Woof!')
+        pass
 
 
 new_dog = Dog('Rex', 'SuperDog')
 print(new_dog.breed)
+new_dog.bark()
